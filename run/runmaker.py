c4184535dfa68f08c09978b073ca37eccfda2368
David Brooks <dbrnz@users.noreply.github.com>
2026-06-16 12:47:34 +1200

    Use a better name to refer to the root path for flatmap sources.

--- run/runmaker.py
+++ run/runmaker.py
@@ -8,13 +8,13 @@ import sys
 
 #===============================================================================
 
-FLATMAP_SOURCE_DIR = os.environ.get('FLATMAP_SOURCE_DIR')
-if FLATMAP_SOURCE_DIR is None:
-    logging.error('FLATMAP_SOURCE_DIR is not set...')
+FLATMAP_SOURCE_ROOT = os.environ.get('FLATMAP_SOURCE_ROOT')
+if FLATMAP_SOURCE_ROOT is None:
+    logging.error('FLATMAP_SOURCE_ROOT is not set...')
     exit(1)
-FLATMAP_SOURCE_DIR = Path(FLATMAP_SOURCE_DIR).resolve()
+FLATMAP_SOURCE_ROOT = Path(FLATMAP_SOURCE_ROOT).resolve()
 
-LOCAL_SOURCE_DIR = Path('/maker/sources/')
+LOCAL_SOURCE_ROOT = Path('/maker/sources/')
 
 #===============================================================================
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             if n < (len(args) - 1):
                 source_path = Path(args[n+1]).resolve()
                 try:
-                    local_path = LOCAL_SOURCE_DIR / source_path.relative_to(FLATMAP_SOURCE_DIR)
+                    local_path = LOCAL_SOURCE_ROOT / source_path.relative_to(FLATMAP_SOURCE_ROOT)
                 except ValueError as e:
                     logging.error(e)
                     exit(1)
